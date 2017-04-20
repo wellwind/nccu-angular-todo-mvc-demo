6564c5df2eab64e76753830ab1266e8c70a2d521
wellwind <wellwind@mail2000.com.tw>
2017-04-20 15:57:01 +0800

    建立第一個component並使用內嵌binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TitleComponent } from './title/title.component';
+import { Title2Component } from './title2/title2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleComponent,
+    Title2Component
   ],
   imports: [
     BrowserModule,
